0ff41363b50ea3660f2bd10d03be64309d9f57b9
FIIZYLemon <matherdylan04@gmail.com>
2023-10-22 23:55:57 +0100

    Rotation added on player movement script
    The player can now rotate to view different perspectives on the map

--- Hollowood/Prefabs/player.gd
+++ Hollowood/Prefabs/player.gd
@@ -9,17 +9,11 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -27,5 +21,11 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		
+	
+	if Input.is_action_just_pressed("RotateLeft"):
+		rotation.y += deg_to_rad(90)
+	if Input.is_action_just_pressed("RotateRight"):
+		rotation.y -= deg_to_rad(90)
+	print(rotation.y)
 	move_and_slide()
